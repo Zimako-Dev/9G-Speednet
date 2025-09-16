60c2d2f787ab2a27822110a2af9b0ddcad4c603d
Zimako-Dev <zimakodev@gmail.com>
2025-09-16 09:31:32 +0200

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,6 @@ import Navbar from '@/components/sections/Navbar';
 import AboutHero from '@/components/sections/AboutHero';
 import AboutVision from '@/components/sections/AboutVision';
 import AboutValues from '@/components/sections/AboutValues';
-import AboutTeam from '@/components/sections/AboutTeam';
-import AboutHistory from '@/components/sections/AboutHistory';
 import Contact from '@/components/sections/Contact';
 import Footer from '@/components/sections/Footer';
 
@@ -15,8 +13,6 @@ export default function AboutPage() {
         <AboutHero />
         <AboutVision />
         <AboutValues />
-        <AboutHistory />
-        <AboutTeam />
         <Contact />
         <Footer />
       </main>
